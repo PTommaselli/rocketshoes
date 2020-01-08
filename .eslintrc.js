f85fff3cdc49f4e3f3879e9952f57d39fb604bf4
pedrotommaselli@gmail.com <pedrotommaselli@gmail.com>
2020-01-08 11:48:35 -0400

    update hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -37,5 +38,7 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'no-param-reassign': 'off',
     'no-console': ["error", { allow: ["tron"] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
